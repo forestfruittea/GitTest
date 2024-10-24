2538d23754eaf3a46cb4469e827986205faf9ea6
Maksimilian <maximka2002r@gmail.com>
2024-10-24 17:22:51 +0300

    bcg

---
+++ testovoe2/src/main/java/crynet/testovoe2/Testovoe2Application.java
@@ -0,0 +1,13 @@
+package crynet.testovoe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testovoe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testovoe2Application.class, args);
+    }
+
+}
---
+++ testovoe2/src/test/java/crynet/testovoe2/Testovoe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package crynet.testovoe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testovoe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
